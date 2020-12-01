bb999fd50ad77da7f759f38307d9d531f60cf09c
Kunal Mehta <legoktm@member.fsf.org>
2020-11-30 16:16:30 -0800

    docker: Switch back to Daimona's repo for phan-taint-check demo
    Bug: T257301
    Depends-On: Iad577169df6187492d6deff001fa2528525e3479
    Change-Id: Ib25ecdc1bc91d01f573816e6cd552b57714cbff2

--- dockerfiles/mediawiki-phan-taint-check-demo/run.sh
+++ dockerfiles/mediawiki-phan-taint-check-demo/run.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 source /srv/emsdk/emsdk_env.sh
 
 # TODO: move this to Gerrit?
-git clone --depth=1 https://github.com/legoktm/demo -b patch-1 /srv/demo
+git clone --depth=1 https://github.com/Daimona/demo /srv/demo
 
 cp -r /src /srv/demo/phan-taint-check-plugin
 cd /srv/demo
