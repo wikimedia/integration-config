b77e3655b6a0d08f7fba2d364983bf9af7aad950
Antoine Musso <hashar@free.fr>
2021-05-10 10:47:29 +0200

    dockerfiles: gerrit move GERRIT_CACHE_HOME in image
    Change-Id: I9073bce698aec5477f62b5a3037b5e92502537ac

--- dockerfiles/gerrit/run.sh
+++ dockerfiles/gerrit/run.sh
@@ -8,9 +8,5 @@ cd /src
 
 bazelisk version
 
-# Setting home dir for Gerrit tools/download_file.py which uses ~ for caching
-# downloaded artifacts
-export GERRIT_CACHE_HOME="$XDG_CACHE_HOME"/gerrit
-
 bazelisk info
-bazelisk build --action_env=GERRIT_CACHE_HOME "${@:-release}"
+bazelisk build "${@:-release}"
