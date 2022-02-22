08edc8356b2af758fb407dfc2c4f6cf5ffddf6e1
Antoine Musso <hashar@free.fr>
2022-02-22 16:41:48 +0100

    dockerfiles: shellcheck: separated glob from grep options
    Change-Id: I1d1bb8c293c4633f59d512f1798eb90790ff9930

--- dockerfiles/shellcheck/run.sh
+++ dockerfiles/shellcheck/run.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 cd /src
 
 # Run against all scripts with a /bin/bash or /bin/sh shebang
-grep -RE '#!/bin/(ba)?sh' ** -l | xargs -n1 -exec shellcheck
+grep -l -RE '#!/bin/(ba)?sh' -- ** | xargs -n1 -exec shellcheck
