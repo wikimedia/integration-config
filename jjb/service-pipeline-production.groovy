120a398f8d48ecd0950fcdb41fa6f184ea2cedde
Dan Duvall <dduvall@wikimedia.org>
2022-05-05 15:42:46 -0700

    jjb: Remove fully deprecated service-pipeline-* jobs
    Jobs deleted:
    * trigger-service-pipeline-test
    * service-pipeline-test
    * trigger-service-pipeline-test-and-publish
    * service-pipeline-test-and-publish
    * trigger-service-jenkinsfile
    * service-jenkinsfile
    
    Bug: T307507
    Change-Id: Ia0c3b407e07576b0ed9276b1d9d5637c4ead4347

--- jjb/service-pipeline-production.groovy
+++
@@ -1,3 +0,0 @@
-def pushProductionImage = true
-def testProductionImage = true
-def nodeLabel = 'blubber && dockerPublish'
--- jjb/service-pipeline-test.groovy
+++
@@ -1,3 +0,0 @@
-def pushProductionImage = false
-def testProductionImage = false
-def nodeLabel = 'blubber'
