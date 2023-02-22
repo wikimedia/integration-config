638b4dc51c64de739728faa4033f2a30c616b105
James D. Forrester <jforrester@wikimedia.org>
2023-02-21 20:19:12 -0500

    Zuul: Migrate all MediaWiki-land node jobs to node16
    Bug: T314470
    Change-Id: I5ee674faa77523a4a61e6c66eec6dc467d8bbabe

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -1156,7 +1156,7 @@ class TestZuulScheduler(unittest.TestCase):
             'wmf-quibble-vendor-mysql-php74-docker': False,
             'wmf-quibble-core-vendor-mysql-php74-docker': True,
             'wmf-quibble-selenium-php74-docker': True,
-            'mwgate-node14-docker': True,
+            'mwgate-node16-docker': True,
         }
         expected_gate = {
             'mediawiki-core-php74-phan-docker': True,
@@ -1176,7 +1176,7 @@ class TestZuulScheduler(unittest.TestCase):
             'wmf-quibble-selenium-php81-docker': True,
             'wmf-quibble-core-vendor-mysql-php74-docker': True,
             'wmf-quibble-core-vendor-mysql-php81-docker': True,
-            'mwgate-node14-docker': True,
+            'mwgate-node16-docker': True,
             'quibble-vendor-mysql-php74-phpunit-standalone-docker': True,
         }
 
