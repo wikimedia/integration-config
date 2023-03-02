77702ee5be0cf11d227096e8e1d81d07099355fe
Jeena Huneidi <jhuneidi@wikimedia.org>
2023-03-02 14:46:33 -0800

    dockerfiles/release-notes: releases branch rename
    Uses the new default branch for the releases repo: 'main'
    
    Bug: T329770
    Change-Id: I952984a8dd1b501740134f081740b62beb362c4a

--- dockerfiles/release-notes/run.sh
+++ dockerfiles/release-notes/run.sh
@@ -40,7 +40,7 @@ echo "$NEW_VERSION" > "$LOG_DIR/VERSION"
 echo "Building Changelog for ${OLD_VERSION}..${NEW_VERSION}"
 
 # Update the releng/release repo, envvar set by dockerfile
-git -C "$RELEASE_TOOLS_DIR" pull --quiet origin master
+git -C "$RELEASE_TOOLS_DIR" pull --quiet origin main
 git -C "$RELEASE_TOOLS_DIR" submodule --quiet update --init --recursive
 
 # Actually make deploy notes
