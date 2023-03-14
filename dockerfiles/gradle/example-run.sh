6ab99125facf0d21cafd8b262402e149dc909ef7
Antoine Musso <hashar@free.fr>
2023-03-14 17:10:06 +0100

    dockerfiles: add example for Gradle image
    Bug: T278203
    Change-Id: Ic95dda4ec665127f0c07f9d3c3ec13b6e57f9b21

---
+++ dockerfiles/gradle/example-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+mkdir -m 2777 -p src
+
+docker run \
+    --rm --tty \
+    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+    --env ZUUL_PROJECT=integration/pipelinelib \
+    --env ZUUL_COMMIT=master \
+    --env ZUUL_REF=master \
+    --volume "/$(pwd)/src://src" \
+        docker-registry.wikimedia.org/releng/ci-src-setup-simple:latest
+
+docker run \
+    --rm --tty \
+    --workdir=/src \
+    --volume /"$(pwd)"/src://src \
+    docker-registry.wikimedia.org/releng/gradle:latest \
+    groovydoc
