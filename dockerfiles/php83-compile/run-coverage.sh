3b7dadff26233b46d83bc5a9bc4973c5a63321a1
James D. Forrester <jforrester@wikimedia.org>
2023-09-01 16:40:04 -0400

    dockerfiles: Provide php83 images based on PHP 8.3.0, gold last week
    Bug: T339350
    Change-Id: I06759ca50b1c1beac54976855b57da1201d87222

---
+++ dockerfiles/php83-compile/run-coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+umask 002
+
+REPORT_PATH=/src/coverage
+INFO_FILE=php-extension.info
+CFLAGS="-g -O0 --coverage -fprofile-arcs -ftest-coverage"
+CXXFLAGS="$CFLAGS"
+LDFLAGS="-lgcov"
+EXTRA_LDFLAGS="-precious-files-regex \.gcno$"
+
+cd src/
+phpize --version
+phpize
+CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" EXTRA_LDFLAGS="$EXTRA_LDFLAGS" ./configure
+
+lcov --directory . --zerocounters
+
+make test
+
+lcov --directory . --capture --output-file $INFO_FILE
+lcov --remove $INFO_FILE "tests/*" "/usr/*" --output-file $INFO_FILE
+genhtml --o $REPORT_PATH -t "${ZUUL_PROJECT} test coverage report" --num-spaces 4 --demangle-cpp $INFO_FILE
