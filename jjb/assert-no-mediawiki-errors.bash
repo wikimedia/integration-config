44a3dc6384e0d4720fbe12ba2bc70c9deda46630
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-10-08 12:28:07 +0200

    Match `split-group` log files when checking for mediawiki errors
    With the introduction of PHPUnit parallel testing, we need to have
    separate log streams for the parallel test runs so that we can debug
    issues that show up under parallel execution. These files include
    `.split-group-{group ID}` (If5e08558a6efbb0139c320fda6f53ec73597ba60).
    
    Modify the `assert-no-mediawiki-errors.bash` script to glob for these
    log files, detecting both the existing linear log files and the new
    split log files.
    
    Bug: T375852
    Change-Id: I4283478a89d2a8d3a0502f836459b3fb21d99113

--- jjb/assert-no-mediawiki-errors.bash
+++ jjb/assert-no-mediawiki-errors.bash
@@ -9,18 +9,20 @@ set -eu
 #  $wgDebugLogGroups['exception'] = "$logDir/mw-error.log";
 #  $wgDebugLogGroups['error'] = "$logDir/mw-error.log";
 #
-ERROR_FILES=( mw-dberror.log mw-error.log )
+# Set patterns to expand to a null string when there is no match
+shopt -s nullglob
+ERROR_FILES=( "$WORKSPACE"/log/mw-dberror*.log "$WORKSPACE"/log/mw-error*.log )
+shopt -u nullglob
 echo "Asserting empty files: ${ERROR_FILES[*]}"
-log_files="$( cd "$WORKSPACE/log"; ls "${ERROR_FILES[@]}" 2> /dev/null || :)"
-if [ ! "$log_files" ]; then
+if [ ! -v ERROR_FILES ]; then
     echo "No error files created by MediaWiki. GOOD"
     exit 0
 fi
-echo "Dumping file(s) $log_files"
+echo "Dumping file(s) ${ERROR_FILES[*]}"
 set +e
     # Use `grep --color . file list` to ensure that file names appear next
     # to log messages
-    (cd "$WORKSPACE/log"; grep --color . "${ERROR_FILES[@]}" 2> /dev/null)
+    grep --color . "${ERROR_FILES[@]}" 2> /dev/null
 set -e
 echo -e "MediaWiki emitted some errors. Check output above."
 exit 1
