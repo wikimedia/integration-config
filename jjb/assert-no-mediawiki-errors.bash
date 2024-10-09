ae319b2a7d461af4fb65f0b43b6f402dcb06aca4
Antoine Musso <hashar@free.fr>
2024-10-09 18:19:08 +0200

    jjb: grep from log dir to have short names
    In 44a3dc6384e the files are found using a glob pattern and I suggested
    to prefix them with "$WORKSPACE"/log which was to also save us from
    having to change directory.  The error files are thus absolute paths and
    that is quite long in the console output.
    
    Change to $WORKSPACE/log when globbing the error files and when doing
    the grep. It will then show the file names relatively to the log dir.
    
    Change-Id: Ic0faa4a95cc43ce14bef3c2ed37599e884d54ba5

--- jjb/assert-no-mediawiki-errors.bash
+++ jjb/assert-no-mediawiki-errors.bash
@@ -9,10 +9,14 @@ set -eu
 #  $wgDebugLogGroups['exception'] = "$logDir/mw-error.log";
 #  $wgDebugLogGroups['error'] = "$logDir/mw-error.log";
 #
+
+pushd "$WORKSPACE/log"
 # Set patterns to expand to a null string when there is no match
 shopt -s nullglob
-ERROR_FILES=( "$WORKSPACE"/log/mw-dberror*.log "$WORKSPACE"/log/mw-error*.log )
+ERROR_FILES=( mw-dberror*.log mw-error*.log )
 shopt -u nullglob
+popd
+
 echo "Asserting empty files: ${ERROR_FILES[*]}"
 if [ ! -v ERROR_FILES ]; then
     echo "No error files created by MediaWiki. GOOD"
@@ -22,7 +26,7 @@ echo "Dumping file(s) ${ERROR_FILES[*]}"
 set +e
     # Use `grep --color . file list` to ensure that file names appear next
     # to log messages
-    grep --color . "${ERROR_FILES[@]}" 2> /dev/null
+    (cd "$WORKSPACE/log"; grep --color . "${ERROR_FILES[@]}" 2> /dev/null)
 set -e
 echo -e "MediaWiki emitted some errors. Check output above."
 exit 1
