01565b366f8dbf11f468d15320a4846324e276d0
Arthur Taylor <arthur.taylor@wikimedia.de>
2025-02-25 15:12:21 +0100

    Fix patch coverage generation for Wikibase
    The `*-coverage` images use `phpunit-patch-coverage` to generate
    coverage information for submitted gerrit patches. This script
    assumes a default PHPUnit test location of `tests/phpunit` in the
    project / extension root.
    
    In the case of Wikibase, this folder does not exist, causing
    `phpunit-patch-coverage` to fail.
    
    Change `mwext-phpunit-coverage-patch.sh` to check to see if the
    `tests/phpunit` folder is present, and in the case that it is not,
    fall back to using the extension / project root.
    
    See also I40c99b4e45ce6cff78c05bbf542ee804a7a8ea48
    
    Bug: T288396
    Change-Id: I07327632ddcb869b4b0d440c89c42883f3313362

--- dockerfiles/quibble-bullseye-php74-coverage/mwext-phpunit-coverage-patch.sh
+++ dockerfiles/quibble-bullseye-php74-coverage/mwext-phpunit-coverage-patch.sh
@@ -17,11 +17,19 @@
 set -eux -o pipefail
 
 EXT_NAME=$(basename "$ZUUL_PROJECT")
-cd "$MW_INSTALL_PATH/extensions/$EXT_NAME"
+EXT_DIR="$MW_INSTALL_PATH/extensions/$EXT_NAME"
+cd "$EXT_DIR"
+
+TEST_DIR="$EXT_DIR/tests/phpunit"
+if [ ! -d "$TEST_DIR" ]; then
+    echo "Folder $TEST_DIR does not exist. Falling back to extension root..."
+    TEST_DIR="$EXT_DIR"
+fi
 
 # Edit suite.xml to use the proper coverage paths
 phpunit-suite-edit "$MW_INSTALL_PATH/tests/phpunit/suite.xml" --cover-extension "$EXT_NAME"
 
 exec phpunit-patch-coverage check \
     --command "php -d extension=pcov.so -d pcov.enabled=1 -d pcov.directory=$PWD -d pcov.exclude='@(tests|vendor)@' -d pcov.initial.files=3000 \"\$MW_INSTALL_PATH\"/tests/phpunit/phpunit.php" \
-    --html "$LOG_DIR"/coverage.html
+    --html "$LOG_DIR"/coverage.html \
+    --test-dir "$TEST_DIR"
