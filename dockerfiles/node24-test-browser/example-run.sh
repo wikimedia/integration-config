2641900dfec7a1f310281ec94b6a52f2b15933c7
James D. Forrester <jforrester@wikimedia.org>
2025-06-03 11:49:39 -0400

    Docker: Provide initial Node 24 images
    Bug: T395923
    Change-Id: I1d10cccd2c05b731454638e061a196b804c11f64

---
+++ dockerfiles/node24-test-browser/example-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -m 777 -p cache log src
+(
+cd src
+git init --quiet
+git fetch --quiet --depth 1 https://gerrit.wikimedia.org/r/data-values/value-view
+git checkout --quiet FETCH_HEAD
+)
+
+docker run \
+    --rm --tty \
+    --volume /"$PWD"/log://var/lib/jenkins/log \
+    --volume /"$PWD"/cache://cache \
+    --volume /"$PWD"/log://log \
+    --volume /"$PWD"/src://src \
+    docker-registry.wikimedia.org/releng/node24-test-browser:latest
