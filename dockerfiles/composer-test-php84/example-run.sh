0410f4f175957c378982c8eaf9369aab99ed768a
Timo Tijhof <krinkle@fastmail.com>
2025-06-20 20:20:34 -0700

    dockerfiles: Re-disable pcov in php81-84 base images
    The incompat with xdebug (whch we use in a few places) makes this
    harder than needed. If we want to solve T395743 with built-in
    phpunit validation for `@covers`, despite upstream PHPUnit declining
    this use case, I guess we'll need to go the way of enabling it in
    the composer-test* images. Or we can give the Phan/PHPCS route a try.
    
    Bug: T395743
    Change-Id: Iaa5e5b6e0562135dbdda2ed336d9b6aa665aacb0

--- dockerfiles/composer-test-php84/example-run.sh
+++ dockerfiles/composer-test-php84/example-run.sh
@@ -6,7 +6,7 @@ mkdir -m 2777 -p log src cache
 (
 cd src
 git init --quiet
-git fetch --quiet --depth 1 "https://gerrit.wikimedia.org/r/integration/jenkins" "master"
+git fetch --quiet --depth 1 "https://gerrit.wikimedia.org/r/RelPath" "master"
 git checkout --quiet FETCH_HEAD
 )
 
@@ -15,4 +15,4 @@ docker run \
     --volume "/$PWD/cache:/cache" \
     --volume "/$PWD/log:/log" \
     --volume "/$PWD/src:/src" \
-    docker-registry.wikimedia.org/releng/composer-test-php73:latest
+    docker-registry.wikimedia.org/releng/composer-test-php84:latest
