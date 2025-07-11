b2dd3c1499db27d94451619db9aaf04cbc5402cc
Antoine Musso <hashar@free.fr>
2025-07-11 14:40:17 +0200

    dockerfiles: add pkg-config to tox-v3-mysqld
    labs/tools/wikinity depends on the mysqlclient Python module which is
    compiled against libmysqld-dev. It requires pkg-config to do so.
    
    Change-Id: I2d8328d6c59ec94efb6e3d5f881b7f35d2a26da6

--- dockerfiles/tox-v3-mysqld/example-run.sh
+++ dockerfiles/tox-v3-mysqld/example-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-install --mode 777 --directory log cache
+install --mode 777 --directory log cache src
 
 # XXX note wikimedia/fundraising/tools test suite expects the Jenkins env
 # variable EXECUTOR_NUMBER to be set.
@@ -10,9 +10,10 @@ docker run \
     --env CI=1 \
     --env EXECUTOR_NUMBER=1 \
     --env ZUUL_URL=https://gerrit.wikimedia.org/r \
-    --env ZUUL_PROJECT=wikimedia/fundraising/tools \
+    --env ZUUL_PROJECT=labs/tools/wikinity \
     --env ZUUL_COMMIT=master \
     --env ZUUL_REF=master \
+    --volume /"$PWD"/src://src \
     --volume /"$PWD"/log://log \
     --volume /"$PWD"/cache://cache \
     docker-registry.wikimedia.org/releng/tox-v3-mysqld:latest
