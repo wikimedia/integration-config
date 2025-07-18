47b75c3e54c3779df3ff0a27dc4d73b2ddd9b279
Antoine Musso <hashar@free.fr>
2025-07-18 13:07:36 +0200

    dockerfiles: composer: remove GitHub oauthtoken management
    Instead of requiring the use of a wrapper that uses `composer config` to
    set the GitHub API token, we have it set from the environment variable
    using:
    
      COMPOSER_AUTH={"github-oauth":{"github.com":"<TOKEN>"}}
    
    Bug: T399846
    Change-Id: Ie8df391a8d2bb6435fe7c0eb9828b8aa0f35d4e6

--- dockerfiles/composer-package-php81/run.sh
+++ dockerfiles/composer-package-php81/run.sh
@@ -6,9 +6,6 @@ set -euxo pipefail
 
 cd /src
 
-if [ -n "${COMPOSER_GITHUB_OAUTHTOKEN}" ]; then
-    composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
-fi
 composer --ansi validate
 composer install --no-progress --prefer-dist
 exec composer test
