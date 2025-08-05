7bb358ac1d354397e63bf5efa0fffe27efd00429
Brett Cornwall <bcornwall@wikimedia.org>
2025-08-05 13:23:12 -0700

    Zuul: Archive operations/debs/trafficserver, again
    This reverts commit e75c89c1b2a94e1a8cbeb38bfbc5b85419fafc9a.
    
    trafficserver has been migrated over to GitLab again.
    
    Change-Id: Ic8adcf533e4f538c97eb2a9c7326adc9d301f673

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -175,12 +175,6 @@ def set_parameters(item, job, params):
             # Nginx support for Encrypted Client Hello (ECH) also needs a
             # specific libssl version - T205378
             params['COMPONENT'] = 'component/nginx-ech'
-        elif (params['ZUUL_PROJECT'] == 'operations/debs/trafficserver'):
-            # Building ATS takes a while
-            params['BUILD_TIMEOUT'] = 60  # minutes
-            # Backports needed on stretch for libbrotli-dev and a recent
-            # debhelper version (>= 11)
-            params['BACKPORTS'] = 'yes'
         elif (params['ZUUL_PROJECT']
               == 'operations/debs/contenttranslation/giella-sme'):
             # Heavy build T143546
