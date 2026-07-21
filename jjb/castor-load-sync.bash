bb2b18a1e00e49c4f5406c2b2ab065b416285b66
Peter Hedenskog <phedenskog@wikimedia.org>
2026-07-21 11:06:26 +0200

    castor: measure cache-load rsync
    Today we measure the castor save time since it has a specific
    job. However we do not know the load time since it is not measured.
    
    This fix follow the same pattern as quibble with start/end
    tags that are used when the log is fetched for the releng server.
    
    This change also make sure we don't swallow if the rsync fails,
    instead we write it to the log.
    
    Add --stats to the rsync so every load reports the number of files
    and bytes moved and we can see the size of every cache on every load.
    
    Bug: T432685
    Change-Id: I8cf0bcf1453b4ddbe0358f0e5d3f6d1b814b44c1

--- jjb/castor-load-sync.bash
+++ jjb/castor-load-sync.bash
@@ -10,12 +10,16 @@ rsync_delete='--delete-delay'
 
 CASTOR_HOST="${CASTOR_HOST:-integration-castor05.integration.eqiad.wmflabs}"
 
-echo "Syncing..."
+echo ">>> Start: castor-load rsync"
+start=$(date +%s%3N)
+section='castor-load rsync'
 rsync \
     --archive \
     ${rsync_delete:-} \
     --delay-updates \
     --contimeout 3 \
+    --stats \
     rsync://"$CASTOR_HOST":/caches/"$CASTOR_NAMESPACE"/ "$DEST" \
-    || :
-echo -e "\nDone"
+    || { echo "castor-load rsync failed (exit code $?); cache is cold"; section='castor-load rsync failed'; }
+ms=$(( $(date +%s%3N) - start ))
+printf '<<< Finish: %s, in %d.%03d s\n' "$section" "$(( ms / 1000 ))" "$(( ms % 1000 ))"
