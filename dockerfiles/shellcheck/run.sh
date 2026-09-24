cfc5081f3df70dc68b28b26c80edab01f8377e10
Antoine Musso <hashar@free.fr>
2026-09-24 19:08:13 +0200

    dockerfiles: [shellcheck] enhance entrypoint command
    Change `grep` to `git grep`, to avoid arsing through `.git` and anything
    that is in the `.gitignore` file (eg node_modules).
    
    Fix regex to match the shebang at the start of line.  No change for
    `mediawiki/tools/code-utils`. For `fresh`, which does not use shellcheck
    yet, it avoids matching the python script bin/fresh-install.
    
    Support matching `/usr/bin/env` as in Fresh `test` and
    `tests/test-fresh-npm`:
    
      #!/usr/bin/env -S -i /bin/bash
    
    Change xargs to pass all files as multiple arguments, saves the overhead
    of starting multiple shellcheck (it is fast, but still).
    
    Change-Id: I71b5f44cf9076b54ed1bc6520e01a9021255370e

--- dockerfiles/shellcheck/run.sh
+++ dockerfiles/shellcheck/run.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 cd /src
 
 # Run against all scripts with a /bin/bash or /bin/sh shebang
-grep -l -RE '#!/bin/(ba)?sh' -- ** | xargs -n1 -exec shellcheck
+git grep -l -E '^#!(/bin/(ba)?sh|/usr/bin/env( -[^ ]+)* /bin/(ba)?sh)' | xargs -exec shellcheck
